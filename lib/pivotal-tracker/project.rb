29addcbe7ae0de49fcfb107fa67eff6fc3fa8cec
jsmestad <justin.smestad@gmail.com>
2010-02-17 10:37:15 -0700

    starting point for new api

--- lib/pivotal-tracker/project.rb
+++ lib/pivotal-tracker/project.rb
@@ -1,10 +1,28 @@
-require 'happymapper'
+module PivotalTracker
+  class Project
 
-class Project
-  include HappyMapper
-  element :id, Integer
-  element :name, String
-  element :iteration_length, Integer
-  element :week_start_day, String
-  element :point_scale, String
+    class << self
+      def all
+        # /projects
+        # Parse each and create new project array
+        # response.each {|x| projects << parse(x) }
+        #parse(connection['/projects'].get)
+        Client.connection['/projects'].get
+      end
+
+      def find(id)
+        # http://www.pivotaltracker.com/services/v2/projects/#{id}
+        # parse response
+        #parse(connection['/projects/#{id}'])
+        Client.connection["/projects/#{id}"].get
+      end
+    end
+
+    attr_accessor :stories
+
+    def initialize
+      self.stories ||= []
+    end
+
+  end
 end
--- lib/pivotal-tracker/story.rb
+++ lib/pivotal-tracker/story.rb
@@ -1,37 +1,14 @@
-class Story
-  include HappyMapper
-  element :id, Integer
-  element :story_type, String
-  element :url, String
-  element :estimate, Integer
-  # possible states: unscheduled, unstarted, started, finished, accepted, rejected
-  element :current_state, String
-  element :name, String
-  element :requested_by, String
-  element :owned_by, String
-  element :created_at, DateTime
-  element :accepted_at, DateTime
-  element :labels, String
-  element :description, String
-  has_one :iteration, Iteration
+module PivotalTracker
+  class Story
+    attr_accessor :project
 
-  def initialize(attributes = {})
-    attributes.each do |key, value|
-      send("#{key}=", value)
+    def initialize(project_id)
+      self.project = project_id
     end
-  end
 
-  def to_xml(options = {})
-    builder = Builder::XmlMarkup.new(options)
-    builder.story do |story|
-      Story.elements.each do |element_type|
-        element = send(element_type.name)
-        eval("story.#{element_type.name}(\"#{element.to_s.gsub("\"", "\\\"")}\")") if element
-      end
+    def all
+      Client.connection["/projects/#{project}/stories"].get
     end
-  end
 
-  def to_param
-    id.to_s
   end
 end
