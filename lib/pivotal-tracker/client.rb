682f98feb3f263b35521bf692e0207fc820b3fa2
jsmestad <justin.smestad@gmail.com>
2010-03-02 09:05:38 -0700

    more Client refactoring

--- lib/pivotal-tracker/client.rb
+++ lib/pivotal-tracker/client.rb
@@ -3,30 +3,32 @@ module PivotalTracker
 
     class << self
       attr_writer :use_ssl, :token
-    end
 
-    def self.use_ssl
-      @use_ssl || false
-    end
-
-    def self.token(username, password, method='post')
-      response = if method == 'post'
-        RestClient.post 'https://www.pivotaltracker.com/services/v3/tokens/active', :username => username, :password => password
-      else
-        RestClient.get "https://#{username}:#{password}@www.pivotaltracker.com/services/v3/tokens/active"
+      def use_ssl
+        @use_ssl || false
       end
-      @token ||= Nokogiri::XML(response).search('guid').inner_html
-    end
 
-    # TODO: document with YARD
-    # this is your connection for the entire module
-    def self.connection(options={})
-      @connection ||= RestClient::Resource.new("#{protocol}://www.pivotaltracker.com/services/v3", :headers => {'X-TrackerToken' => @token, 'Content-Type' => 'application/xml'})
-    end
+      def token(username, password, method='post')
+        response = if method == 'post'
+          RestClient.post 'https://www.pivotaltracker.com/services/v3/tokens/active', :username => username, :password => password
+        else
+          RestClient.get "https://#{username}:#{password}@www.pivotaltracker.com/services/v3/tokens/active"
+        end
+        @token ||= Nokogiri::XML(response).search('guid').inner_html
+      end
 
-    protected
-    def self.protocol
-      use_ssl ? 'https' : 'http'
+      # this is your connection for the entire module
+      def connection(options={})
+        @connection ||= RestClient::Resource.new("#{protocol}://www.pivotaltracker.com/services/v3", :headers => {'X-TrackerToken' => @token, 'Content-Type' => 'application/xml'})
+      end
+
+      protected
+    
+        def protocol
+          use_ssl ? 'https' : 'http'
+        end
+      
     end
+    
   end
 end
