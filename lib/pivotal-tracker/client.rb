23b5aa59a08699cddd6c352e0c5368c97a9ab12b
Tim Linquist <timo3377@gmail.com>
2010-09-17 11:54:04 -0700

    Updated token() accessor to use attr_writer

--- lib/pivotal-tracker/client.rb
+++ lib/pivotal-tracker/client.rb
@@ -9,12 +9,13 @@ module PivotalTracker
       end
 
       def token(username, password, method='post')
+        return @token if @token
         response = if method == 'post'
           RestClient.post 'https://www.pivotaltracker.com/services/v3/tokens/active', :username => username, :password => password
         else
           RestClient.get "https://#{username}:#{password}@www.pivotaltracker.com/services/v3/tokens/active"
         end
-        @token ||= Nokogiri::XML(response.body).search('guid').inner_html
+        @token= Nokogiri::XML(response.body).search('guid').inner_html
       end
 
       # this is your connection for the entire module
