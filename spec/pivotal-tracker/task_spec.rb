eb36fa6dfdaef4f770b53717f6a6637c7ef9e59b
Douglas Sellers <douglas.sellers@gmail.com>
2012-04-30 22:56:15 -0700

    Fixed a small bug in the way that tasks are associated with parent stories

--- spec/pivotal-tracker/task_spec.rb
+++ spec/pivotal-tracker/task_spec.rb
@@ -18,4 +18,11 @@ describe PivotalTracker::Task do
       @story.tasks.find(468113).should be_a(PivotalTracker::Task)
     end
   end
+
+  context ".create" do
+    it "should return the created task" do
+      @story.tasks.create(:description => 'Test task')
+    end
+  end
+    
 end
