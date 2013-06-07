93baf34f8f19b2d6d58082779d0d62a248f39294
Hannah Howard <hannah@hannahhoward.net>
2013-06-07 15:04:21 -0700

    Add paging ability

--- lib/pivotal-tracker/activity.rb
+++ lib/pivotal-tracker/activity.rb
@@ -2,10 +2,21 @@ module PivotalTracker
   class Activity
     include HappyMapper
     class << self
+
+      MAX_PIVOTAL_ACTIVITY = 100
+
       def all(project=nil, options={})
+        per_page = options[:limit] || MAX_PIVOTAL_ACTIVITY
+        auto_page = options[:auto_page]
+
         params = self.encode_options(options)
+
         if project
-          parse(Client.connection["/projects/#{project.id}/activities#{params}"].get)
+          if auto_page
+            page_through_activities(project, params, per_page)
+          else
+            parse(Client.connection["/projects/#{project.id}/activities#{params}"].get)
+          end
         else
           parse(Client.connection["/activities#{params}"].get)
         end
@@ -17,7 +28,7 @@ module PivotalTracker
           return nil if !options.is_a?(Hash) || options.empty?
 
           options_string = []
-          options_string << "limit=#{options.delete(:limit)}" if options[:limit]
+          options_string << "limit=#{options.delete(:limit)}" 
           options_string << "newer_than_version=#{options.delete(:newer_than_version)}" if options[:newer_than_version]
 
           if options[:occurred_since]
@@ -27,6 +38,17 @@ module PivotalTracker
           end
 
           return "?#{options_string.join('&')}"
+        end
+
+        def page_through_activities(project, params, per_page)
+            collections = []
+            page = 1
+            begin
+              collections << parse(Client.connection["/projects/#{project.id}/activities#{params}&page=#{page}"].get)
+              returned = collections.last.size
+              page += 1
+            end while returned == per_page
+            collections.flatten
         end
 
     end
